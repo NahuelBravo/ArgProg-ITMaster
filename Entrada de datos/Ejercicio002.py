08d3d40b06599cdcd0182eef6e5086fd994c0bac
Nahuel Bravo <nahuelbravo05@gmail.com>
2023-09-18 09:39:49 -0300

    commit insano

--- Entrada de datos/Ejercicio002.py
+++
@@ -1,9 +0,0 @@
-"""
-Escribir un programa que solicite al usuario su nombre y su edad, 
-y luego muestre por pantalla un mensaje que diga "Hola, [nombre]. Tu edad es [edad] a�os."
-"""
-
-nombre = input("Ingrese su nombre: ")
-edad = int(input("Ingrese su edad: "))
-
-print("Hola, " + nombre + ". Tu edad es " + str(edad) + " años.")
--- Flujos de repeticion/Ejercicio039.py
+++
@@ -1,8 +0,0 @@
-"""
-Escribir un programa para calcular e imprimir la suma de los n�meros comprendidos entre 42 y 176
-
-"""
-
-for i in range(42, 177):
-    j = i + 1
-    print(f"{i} + {j} = {i + j}")
